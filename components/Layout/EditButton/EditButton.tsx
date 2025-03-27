f009b8b5c930877dc6eea8002ec34a9f3e4ccd0f
Maciej Wójcik <maciek@wjck.pl>
2025-03-27 11:03:53 +0100

    adjust edit modal z-index

--- components/Layout/EditButton/EditButton.tsx
+++ components/Layout/EditButton/EditButton.tsx
@@ -98,6 +98,7 @@ export const EditButton = ({
                 position: strategy,
                 top: y ?? 0,
                 left: x ?? 0,
+                zIndex: 3,
               }}
               onClick={(env) => {
                 env.stopPropagation();
