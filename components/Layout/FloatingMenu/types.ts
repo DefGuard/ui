4fa89706fbdf91c83b43850145a4cf8152b5f471
Filip Ślęzak <fslezak@teonite.com>
2025-04-10 14:30:13 +0200

    add floating menu component

---
+++ components/Layout/FloatingMenu/types.ts
@@ -0,0 +1,12 @@
+import { Placement } from '@floating-ui/react';
+
+import { useFloatingMenu } from './useFloatingMenuContext';
+
+export type FloatingMenuContextOptions = {
+  initialOpen?: boolean;
+  placement?: Placement;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+};
+
+export type FloatingMenuContextType = ReturnType<typeof useFloatingMenu> | null;
