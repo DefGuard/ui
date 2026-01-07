7fcd57bb93d4db39d62db658f728dc043f5431f9
Filip Ślęzak <fslezak@teonite.com>
2026-01-07 13:03:17 +0100

    update loader spinner

--- components/LoaderSpinner/LoaderSpinner.tsx
+++ components/LoaderSpinner/LoaderSpinner.tsx
@@ -1,13 +1,15 @@
+import clsx from 'clsx';
 import { Icon } from '../Icon';
 import './style.scss';
 
 type Props = {
   size?: number;
+  variant?: 'empty' | 'primary';
 };
 
-export const LoaderSpinner = ({ size = 20 }: Props) => {
+export const LoaderSpinner = ({ size = 20, variant = 'empty' }: Props) => {
   return (
-    <div className="loader-spinner">
+    <div className={clsx('loader-spinner', `variant-${variant}`)}>
       <Icon icon="loader" size={size} />
     </div>
   );
