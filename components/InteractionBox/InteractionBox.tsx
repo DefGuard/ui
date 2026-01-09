88e91c9f022249b0302d12c323d8c857032557eb
Filip Ślęzak <fslezak@teonite.com>
2026-01-09 15:47:29 +0100

    add snackbars

--- components/InteractionBox/InteractionBox.tsx
+++ components/InteractionBox/InteractionBox.tsx
@@ -1,4 +1,4 @@
-import type { MouseEventHandler, Ref } from 'react';
+import { type MouseEventHandler, type Ref, useMemo } from 'react';
 import { Icon } from '../Icon';
 import type { IconKindValue } from '../Icon/icon-types';
 import './style.scss';
@@ -6,6 +6,7 @@ import clsx from 'clsx';
 
 type Props = {
   iconSize: number;
+  interactionSize?: number;
   icon: IconKindValue;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   id?: string;
@@ -23,10 +24,19 @@ export const InteractionBox = ({
   id,
   ref,
   tabIndex,
+  interactionSize,
   disabled = false,
 }: Props) => {
+  const style = useMemo(() => {
+    const res: Record<string, number | string> = {};
+    if (interactionSize) {
+      res['--interaction-size'] = `${interactionSize}px`;
+    }
+    return res;
+  }, [interactionSize]);
+
   return (
-    <div className={clsx('interaction-box', className)} ref={ref} id={id}>
+    <div className={clsx('interaction-box', className)} ref={ref} id={id} style={style}>
       <Icon icon={icon} size={iconSize} />
       <button
         type="button"
--- components/LoaderSpinner/LoaderSpinner.tsx
+++ components/LoaderSpinner/LoaderSpinner.tsx
@@ -1,15 +1,23 @@
 import clsx from 'clsx';
 import { Icon } from '../Icon';
 import './style.scss';
+import { useMemo } from 'react';
 
 type Props = {
   size?: number;
   variant?: 'empty' | 'primary';
 };
 
-export const LoaderSpinner = ({ size = 20, variant = 'empty' }: Props) => {
+export const LoaderSpinner = ({ size = 20, variant }: Props) => {
+  const variantClass = useMemo(() => (variant ? `variant-${variant}` : null), [variant]);
   return (
-    <div className={clsx('loader-spinner', `variant-${variant}`)}>
+    <div
+      className={clsx('loader-spinner', variantClass)}
+      style={{
+        height: size,
+        width: size,
+      }}
+    >
       <Icon icon="loader" size={size} />
     </div>
   );
