658678b6740b36e99b9bd86b3f3bb638ad35e625
Jacek Chmielewski <jacek@defguard.net>
2026-03-11 12:10:50 +0100

    add "copy" button to CodeCard component

--- components/CodeCard/CodeCard.tsx
+++ components/CodeCard/CodeCard.tsx
@@ -1,12 +1,29 @@
 import './style.scss';
 import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 import { ThemeVariable } from '../../types';
+import { useClipboard } from '../../hooks/useClipboard';
 import { isPresent } from '../../utils/isPresent';
 import { Icon } from '../Icon';
 import { IconButton } from '../IconButton/IconButton';
 import type { CodeCardProps } from './types';
 
-export const CodeCard = ({ title, value, onCopy, onDownload }: CodeCardProps) => {
+export const CodeCard = ({ title, value, copy, onDownload }: CodeCardProps) => {
+  const [copied, setCopied] = useState(false);
+  const { writeToClipboard } = useClipboard();
+
+  useEffect(() => {
+    if (!copied) return;
+
+    const timeout = setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [copied]);
+
   return (
     <div className={clsx('code-card')}>
       <div className="top">
@@ -14,7 +31,16 @@ export const CodeCard = ({ title, value, onCopy, onDownload }: CodeCardProps) =>
           <Icon icon="code" staticColor={ThemeVariable.FgMuted} />
           <p className="card-title">{title}</p>
           <div className="actions">
-            {isPresent(onCopy) && <IconButton onClick={onCopy} icon="copy" />}
+            {copy && (
+              <IconButton
+                onClick={() => {
+                  void writeToClipboard(value);
+                  setCopied(true);
+                }}
+                icon={!copied ? 'copy' : 'check-filled'}
+                className={clsx({ copied })}
+              />
+            )}
             {isPresent(onDownload) && <IconButton onClick={onDownload} icon="download" />}
           </div>
         </div>
--- components/CodeCard/types.ts
+++ components/CodeCard/types.ts
@@ -1,6 +1,6 @@
 export interface CodeCardProps {
   value: string;
   title: string;
-  onCopy?: () => void;
+  copy?: boolean;
   onDownload?: () => void;
 }
