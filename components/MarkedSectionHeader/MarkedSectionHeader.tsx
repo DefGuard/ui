ef38ed5d3578ae1901f7e0ed87fe15e99e994cf3
Maciek <19913370+wojcik91@users.noreply.github.com>
2026-04-03 10:43:56 +0200

    add optional warning banner to form section header (#34)

--- components/MarkedSectionHeader/MarkedSectionHeader.tsx
+++ components/MarkedSectionHeader/MarkedSectionHeader.tsx
@@ -1,19 +1,27 @@
 import { TextStyle, ThemeSpacing, ThemeVariable } from '../../types';
 import { AppText } from '../AppText/AppText';
+import { InfoBanner } from '../InfoBanner/InfoBanner';
 import { SizedBox } from '../SizedBox/SizedBox';
 
 type Props = {
   title: string;
   description: string;
+  warning?: string;
 };
 
-export const MarkedSectionHeader = ({ description, title }: Props) => {
+export const MarkedSectionHeader = ({ description, title, warning }: Props) => {
   return (
     <>
       <AppText font={TextStyle.TBodyPrimary600} color={ThemeVariable.FgDefault}>
         {title}
       </AppText>
       <SizedBox height={ThemeSpacing.Xl} />
+      {warning && (
+        <>
+          <InfoBanner variant="warning" icon="info-outlined" text={warning} />
+          <SizedBox height={ThemeSpacing.Xl} />
+        </>
+      )}
       <AppText font={TextStyle.TBodySm400} color={ThemeVariable.FgMuted}>
         {description}
       </AppText>
