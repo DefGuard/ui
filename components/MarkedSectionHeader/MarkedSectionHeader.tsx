715228a4f33b0de31195ca10c47d85a97cb23b97
Maciej Wójcik <maciej@defguard.net>
2026-06-22 11:43:45 +0200

    add optional badge to section header

--- components/MarkedSectionHeader/MarkedSectionHeader.tsx
+++ components/MarkedSectionHeader/MarkedSectionHeader.tsx
@@ -1,20 +1,33 @@
 import { TextStyle, ThemeSpacing, ThemeVariable } from '../../types';
+import { isPresent } from '../../utils/isPresent';
 import { AppText } from '../AppText/AppText';
+import { Badge } from '../Badge/Badge';
+import type { BadgeProps } from '../Badge/types';
 import { InfoBanner } from '../InfoBanner/InfoBanner';
 import { SizedBox } from '../SizedBox/SizedBox';
+import './style.scss';
 
 type Props = {
   title: string;
   description?: string;
   warning?: string;
+  badgeProps?: BadgeProps;
 };
 
-export const MarkedSectionHeader = ({ description, title, warning }: Props) => {
+export const MarkedSectionHeader = ({
+  description,
+  title,
+  warning,
+  badgeProps,
+}: Props) => {
   return (
     <>
-      <AppText font={TextStyle.TBodyPrimary600} color={ThemeVariable.FgDefault}>
-        {title}
-      </AppText>
+      <div className="marked-section-header">
+        <AppText font={TextStyle.TBodyPrimary600} color={ThemeVariable.FgDefault}>
+          {title}
+        </AppText>
+        {isPresent(badgeProps) && <Badge {...badgeProps} />}
+      </div>
       {description && (
         <>
           <SizedBox height={ThemeSpacing.Xl} />
